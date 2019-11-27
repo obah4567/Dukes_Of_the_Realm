406c4dbc30d68c48a7817460a55193357b443522
obah4567 <56270658+obah4567@users.noreply.github.com>
2019-11-27 14:35:35 +0100

    Add files via upload
    Modif de la main pour afficher un chateau carré

--- POO/src/SampleGame/Main.java
+++ POO/src/SampleGame/Main.java
@@ -1,11 +1,10 @@
-package SampleGame;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-import SampleGame.alien.Input;
+//import SampleGame.alien.Input;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -15,20 +14,30 @@ import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 public class Main extends Application {
 	private Random rnd = new Random();
 
 	private Pane playfieldLayer, layerFieldChateau;
+	private ImageView imageViewChateau;
+	
+	private AnimationTimer gameLoop;
 	
 	private List<Chateau> chateau = new ArrayList<>();
 
 	private Image chateauNeutreImage;
 	private Image chateauNormalImage;
-
+	private Duc duc;
+	
+	private Player player;
+	
+	private static Settings settings = new Settings();
 
 	private Scene scene;
 	private Input input;
@@ -40,18 +49,20 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
+		primaryStage.setTitle("Dukes of the Realm");
 		root = new Group();
-		scene = new Scene(root, Settings.SCENE_WIDTH, Settings.SCENE_HEIGHT + Settings.STATUS_BAR_HEIGHT);
-		scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
+		scene = new Scene(root, Settings.SCENE_WIDTH, Settings.SCENE_HEIGHT + Settings.STATUS_BAR_HEIGHT, Color.WHITE);
+		//scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
 
-		// create layers
-		playfieldLayer = new Pane();
-		root.getChildren().add(playfieldLayer);
 		
-		loadGame();
+		// create layers
+		layerFieldChateau = new Pane();
+		root.getChildren().add(layerFieldChateau);
+		
+		//loadGame();
 		
 		gameLoop = new AnimationTimer() {
 			@Override
@@ -82,6 +93,35 @@ public class Main extends Application {
 		}
 
 	};
+	
+	 Rectangle chateau_DUC = new Rectangle();
+	 Rectangle porte_Duc = new Rectangle();
+	 Rectangle barron1 = new Rectangle();
+	 Rectangle barron2 = new Rectangle();
+	 
+	 
+	 chateau_DUC.setX(50);
+	 chateau_DUC.setY(25);
+	 chateau_DUC.setWidth(150);
+	 chateau_DUC.setHeight(150);
+	 chateau_DUC.setFill(Color.GREEN);
+	 /////
+	 porte_Duc.setX(100);
+	 porte_Duc.setY(160);
+	 porte_Duc.setWidth(40);
+	 porte_Duc.setHeight(20);
+	 porte_Duc.setFill(Color.BLACK);
+	 /////
+	 
+	 
+	    
+	    
+	    root.getChildren().add(chateau_DUC);//On ajoute le rectangle après le cercle
+	    //primaryStage.setVisible(true);
+	    
+	    
+	    root.getChildren().add(porte_Duc);//On ajoute le rectangle après le cercle
+	    //primaryStage.setVisible(true);
 	}
 
 	
@@ -95,18 +135,20 @@ public class Main extends Application {
 		{
 			int dx = aleatoire.nextInt(Settings.SCENE_WIDTH - 50) + 50;
 			int dy = aleatoire.nextInt(Settings.SCENE_HEIGHT - 50) + 50;
-			tab[i] = new Chateau(imageViewChateau, layerFieldChateau, LIST[i], dx, dy);
 			
+			tab[i] = new Chateau_Normal(layerFieldChateau, chateauNeutreImage, duc , dx, dy);
+
+					
 		}
 		return tab;
 	}
 	
+   
 	
 	
-	
-	private void loadGame() {
+    private void loadGame() {
 		
-		chateauNormalImage = new Image(getClass().getResource("/images/chateau_image.gif").toExternalForm(), 100, 100, true, true);
+		//chateauNormalImage = new Image(getClass().getResource("/images/chateau_image.gif").toExternalForm(), 100, 100, true, true);
 		chateauNeutreImage = new Image(getClass().getResource("/images/enemy.png").toExternalForm(), 50, 50, true, true);		
 		//createPlayer();
 		
@@ -115,25 +157,10 @@ public class Main extends Application {
 	private void createPlayer() {
 		double x = (Settings.SCENE_WIDTH - chateauNormalImage.getWidth()) / 2.0;
 		double y = Settings.SCENE_HEIGHT * 0.7;
-		Player = new Player(layerFieldChateau , chateauNormalImage, x,y);
+		player = new Player(layerFieldChateau , chateauNormalImage, x,y, 1 ,1.0);
 		
-		player.getView().setOnMousePressed(e -> {
-			System.out.println("Click on player");
-			e.consume();
-		});
-		
-		player.getView().setOnContextMenuRequested(e -> {
-			ContextMenu contextMenu = new ContextMenu();
-			//MenuItem low = new MenuItem("Slow");
-			//MenuItem medium= new MenuItem("Regular");
-			//MenuItem high= new MenuItem("Fast");
-			/*low.setOnAction(evt -> player.setFireFrequencyLow());
-			medium.setOnAction(evt -> player.setFireFrequencyMedium());
-			high.setOnAction(evt -> player.setFireFrequencyHigh());
-			contextMenu.getItems().addAll(low, medium, high);
-			contextMenu.show(player.getView(), e.getScreenX(), e.getScreenY());*/
-		});
 	}
+	
 	private void processInput(Input input, long now) {
 		if (input.isPause())
 		{
